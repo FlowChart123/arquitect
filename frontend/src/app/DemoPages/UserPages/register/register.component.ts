e26e50449f44a899d72f7993777c8d3ce004b59e
FlowChart123 <104113979+FlowChart123@users.noreply.github.com>
2023-08-09 02:21:19 -0300

    09/08/2023

--- frontend/src/app/DemoPages/UserPages/register/register.component.ts
+++ frontend/src/app/DemoPages/UserPages/register/register.component.ts
@@ -43,9 +43,9 @@ export class RegisterComponent implements OnInit {
     this.myForm = this.fb.group({
       cpf:['',[Validators.required, GenericValidator.isValidCpf()]],
       email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+      senha: ['', [Validators.required]],
       confirmPassword: ['']
-    }, { validators: [ConfirmValidator('password','confirmPassword')]   })
+    }, { validators: [ConfirmValidator('senha','confirmPassword')]   })
 
   }
 
@@ -54,16 +54,19 @@ export class RegisterComponent implements OnInit {
   get f(){
     return this.myForm.controls;
   }
-
-
+  
   Save()
-  {
+  {    
     if (this.myForm.valid==true){
-      let data = this.myForm.value;
+      let data = this.myForm.value;      
       this.loginService.createUser(data).subscribe(p=>
-        {
-          console.log(p);
-          this.router.navigate(["login"]);
+        {               
+          if (p!='Usuário Adicionado!'){
+            alert('Erro ao adicionar');
+          }    
+          else{
+            this.router.navigate(["pages/login"]);
+          }                 
         })
     }
   }
